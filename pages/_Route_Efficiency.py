b6231312df6171cb6a5ec31d58fad58c816d2491
Rovin Lobo <rovinlobo0099@gmail.com>
2026-03-07 23:09:39 +0530

    Refactor route efficiency analysis with data filters
    Refactor route efficiency analysis to apply filters on data before processing. Update the display of average lead time and bottom slowest routes.

--- pages/_Route_Efficiency.py
+++ pages/_Route_Efficiency.py
@@ -1,21 +1,15 @@
 import streamlit as st
 import plotly.express as px
-from utils import load_data
+from utils import load_data, apply_filters
+
+df = apply_filters(load_data())
 
 st.title("🚦 Route Efficiency Analysis")
-
-df = load_data()
 
 route_stats = df.groupby("Route").agg(
 avg_days=("Shipping Days","mean"),
 orders=("Shipping Days","count")
 ).reset_index()
-
-max_days = route_stats["avg_days"].max()
-
-route_stats["Efficiency Score"] = (max_days - route_stats["avg_days"]) / max_days
-
-st.subheader("Average Lead Time by Route")
 
 fig = px.bar(route_stats,x="Route",y="avg_days")
 
