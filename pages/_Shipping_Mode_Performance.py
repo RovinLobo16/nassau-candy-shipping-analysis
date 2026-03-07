2d6fcc51dab20b0a33c29e37014221b48a2b2b47
Rovin Lobo <rovinlobo0099@gmail.com>
2026-03-07 23:10:53 +0530

    Apply filters to shipping mode data before visualization
    Refactor shipping mode performance visualization to apply filters before loading data.

--- pages/_Shipping_Mode_Performance.py
+++ pages/_Shipping_Mode_Performance.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import plotly.express as px
-from utils import load_data
+from utils import load_data, apply_filters
+
+df = apply_filters(load_data())
 
 st.title("🚛 Shipping Mode Performance")
-
-df = load_data()
 
 ship_perf = df.groupby("Ship Mode")["Shipping Days"].mean().reset_index()
 
 fig = px.bar(ship_perf,x="Ship Mode",y="Shipping Days",color="Ship Mode")
-
-st.plotly_chart(fig,use_container_width=True)
-
-fig = px.box(df,x="Ship Mode",y="Shipping Days")
 
 st.plotly_chart(fig,use_container_width=True)
