a4caddf84d26fd35108c3f5a6aa64b26422aa3af
Rovin Lobo <rovinlobo0099@gmail.com>
2026-03-07 19:12:09 +0530

    Refactor factory insights display and add scatter plot
    Updated the title and removed unused product factory data display. Added a geographical scatter plot for factory locations.

--- pages/_Product_Factory_Insights.py
+++ pages/_Product_Factory_Insights.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.title("🏭 Product & Factory Insights")
-
-st.subheader("Factories Coordinates")
+st.title("🏭 Factory Insights")
 
 factory_data = pd.DataFrame({
+
 "Factory":[
 "Lot's O' Nuts",
 "Wicked Choccy's",
@@ -13,54 +13,18 @@ factory_data = pd.DataFrame({
 "Secret Factory",
 "The Other Factory"
 ],
+
 "Latitude":[32.881893,32.076176,48.11914,41.446333,35.1175],
 "Longitude":[-111.768036,-81.088371,-96.18115,-90.565487,-89.97107]
 })
 
 st.dataframe(factory_data)
 
-st.subheader("Products and Factories Correlation")
+fig = px.scatter_geo(
+factory_data,
+lat="Latitude",
+lon="Longitude",
+hover_name="Factory"
+)
 
-product_factory = pd.DataFrame({
-"Division":[
-"Chocolate","Chocolate","Chocolate","Chocolate","Chocolate",
-"Sugar","Sugar","Sugar","Sugar",
-"Other","Sugar","Sugar","Other","Other","Other"
-],
-"Product":[
-"Wonka Bar – Nutty Crunch Surprise",
-"Wonka Bar – Fudge Mallows",
-"Wonka Bar – Scrumdiddlyumptious",
-"Wonka Bar – Milk Chocolate",
-"Wonka Bar – Triple Dazzle Caramel",
-"Laffy Taffy",
-"SweetTARTS",
-"Nerds",
-"Fun Dip",
-"Fizzy Lifting Drinks",
-"Everlasting Gobstopper",
-"Hair Toffee",
-"Lickable Wallpaper",
-"Wonka Gum",
-"Kazookles"
-],
-"Factory":[
-"Lot's O' Nuts",
-"Lot's O' Nuts",
-"Lot's O' Nuts",
-"Wicked Choccy's",
-"Wicked Choccy's",
-"Sugar Shack",
-"Sugar Shack",
-"Sugar Shack",
-"Sugar Shack",
-"Sugar Shack",
-"Secret Factory",
-"The Other Factory",
-"Secret Factory",
-"Secret Factory",
-"The Other Factory"
-]
-})
-
-st.dataframe(product_factory)
+st.plotly_chart(fig,use_container_width=True)
